a472cf1e01ebd31c0eef150255db10c94c05cb0c
qinzhihao <scottzhq@163.com>
2016-12-22 22:37:04 +0800

    implement groupRelationship

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current.user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -41,6 +42,29 @@ class GroupsController < ApplicationController
 
     redirect_to groups_path, alert: "Group deleted"
   end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功"
+    else
+      flash[:warning] = "你已经是本讨论版成员了"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出"
+    else
+      flash[:warning] = "你不是本论坛成员，如何退出XD"
+    end
+    redirect_to group_path(@group)
+  end
+
 
 
       private
