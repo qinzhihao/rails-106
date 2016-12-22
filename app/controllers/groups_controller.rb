c6dcd2fb8b96d7c9096a8e2cd904961c6256e564
qinzhihao <scottzhq@163.com>
2016-12-22 15:24:51 +0800

    implement groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,16 +8,36 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
 
-    redirect_to groups_path
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Updated Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+
+    redirect_to groups_path, alert: "Group deleted"
   end
 
 
@@ -25,7 +45,7 @@ class GroupsController < ApplicationController
 
       def group_params
         params.require(:group).permit(:title, :description)
-      end 
+      end
 
 
 end
